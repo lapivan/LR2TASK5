f2fa54b0f88a5765cd7c3ed2a8783ee4e8902808
ivan <lapunoujohn@gmail.com>
2024-09-16 13:23:14 +0300

    main

---
+++ task_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    double x, y;
+    std::cout << "Enter x: ";
+    std::cin >> x;
+    std::cout << "Enter y: ";
+    std::cin >> y;
+    if (x>y) 
+    std::cout << "x > y" << std::endl;
+    else 
+    std::cout << "x < y" << std::endl; 
+    return 0;
+}
